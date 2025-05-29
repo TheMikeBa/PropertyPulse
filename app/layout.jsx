6748b1a3fe52267e74489d26ac2fe5b567a7b233
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-05-29 00:15:15 -0400

    Initial Commit

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
+
+export const metadata = {
+  title: "Property Pulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
