2e21c96fadaf7c03062a6b1f018958b5ef2d3270
TheMikeBa <21992394+TheMikeBa@users.noreply.github.com>
2025-06-07 11:55:51 -0400

    deployment edits 8

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -39,8 +39,8 @@ const HomeProperties = async () => {
             Recent Properties
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {/* {recentProperties === 0 ? ( */}
-            {recentProperties.length === 0 ? (
+            {recentProperties === 0 ? (
+              // {recentProperties.length === 0 ? (
               <p>No Properties Found</p>
             ) : (
               recentProperties.map((property) => (
